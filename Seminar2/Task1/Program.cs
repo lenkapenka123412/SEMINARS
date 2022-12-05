4b965b8ee22c72ace60adb9301ddb72c40ae5d21
Elena <lenka.penka123412@gmail,com>
2022-12-05 15:08:59 +0300

    Герерация случайного числа

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Task1
+
+Console.Clear();
+int n = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число: {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+if( n1 < n2)
+    Console.WriteLine(n1);
+    else
+    Console.WriteLine(n2);
