881b3a28807af5c0c65ea5301fa4d712f68895de
Elena <lenka.penka123412@gmail,com>
2022-12-05 16:37:52 +0300

    Подсказка к дополнительной задаче

--- Seminar2/Task5/Program.cs
+++ Seminar2/Task5/Program.cs
@@ -5,9 +5,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-if (n * n == m) 
-    Console.WriteLine("Да");
-else if (m * m == n)
+if (n * n == m || m * m == n) 
     Console.WriteLine("Да");
 else
     Console.WriteLine("Нет");
---
+++ Seminar2/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+while( n != 0 )
+{
+     n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("End");
+   
