7cf2a3288a14ee3dcbbf898d8edfed647dc9803c
Elena <lenka.penka123412@gmail,com>
2022-12-05 13:19:07 +0300

    Task3 задача 6

---
+++ Seminar1_DZ/Task3_DZ/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n % 2 == 0)
+Console.WriteLine("Четное");
+else
+Console.WriteLine("Не четное");
