6f3943ea50e23fe2b42c1ecd2b21a7fb88f59755
Elena <lenka.penka123412@gmail,com>
2022-12-05 18:03:38 +0300

    Задча 10

--- Seminar2_DZ/Task1/Program.cs
+++ Seminar2_DZ/Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string m = Convert.ToString(n);
+Console.WriteLine("вторая цифра этого числа -> "+m[1]);
