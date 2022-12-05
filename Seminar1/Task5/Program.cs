17cbd5274d1b9679eb85bf03942b557e8f3f7dd8
Elena <lenka.penka123412@gmail,com>
2022-12-05 12:29:31 +0300

    Task5 Вывод числа от -n до n через цикл while

---
+++ Seminar1/Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Task5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = (-1) * n;
+
+while(i <= n)
+{
+    Console.Write($"{i} ");
+    i++;
+
+}
+    
