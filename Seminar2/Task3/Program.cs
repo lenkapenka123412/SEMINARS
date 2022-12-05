cfe59336d94661734652873f46f832154472780e
Elena <lenka.penka123412@gmail,com>
2022-12-05 15:58:26 +0300

    Определение кратного числа и вывод остатка

---
+++ Seminar2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if(n % m == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Не кратно, остаток {n % m}");
