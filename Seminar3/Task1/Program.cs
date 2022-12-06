b72c831f30a2c7fa6294c9127c5248d658544697
Elena <lenka.penka123412@gmail,com>
2022-12-06 11:08:21 +0300

    comments

---
+++ Seminar3/Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿// task 17
+
+/*Console.Clear();
+Console.Write("Введите координату X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+while (x == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату X: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.Write("Вы ошиблись!\nВведите координату Y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
+*/
+
+// task 18
+
+/*Console.Clear();
+Console.Write("Введите координату X: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1 || n == 2)
+    Console.WriteLine("y > 0");
+else
+    Console.WriteLine("y < 0");
+
+if (n == 1 || n == 4)
+    Console.WriteLine("x > 0");
+else
+    Console.WriteLine("x < 0");*/
+
+// task 21
+
+/*Console.Clear();
+Console.Write("Введите X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine(Math.Round(dist, 2));*/
+
+/*// task 22
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i} ");
+*/
+
+
