2890df9324106bdd991f2637300346976f6713de
Elena <lenka.penka123412@gmail,com>
2022-12-19 21:52:43 +0300

    Задача доп сбор черники

---
+++ Seminar3_DZ/Task4_dop/Program.cs
@@ -0,0 +1,22 @@
+﻿// Доп сбор черники
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 11); // [1, 10]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int maxSum = 0;
+for (int i = 1; i < array.Length - 1; i++)
+{
+if (array[i - 1] + array[i] + array[i + 1] > maxSum)
+maxSum = array[i - 1] + array[i] + array[i + 1];
+}
+
+if (array[0] + array[1] + array[array.Length - 1] > maxSum)
+maxSum = array[0] + array[1] + array[array.Length - 1];
+if (array[0] + array[array.Length - 2] + array[array.Length - 1] > maxSum)
+maxSum = array[0] + array[array.Length - 2] + array[array.Length - 1];
+
+Console.WriteLine(maxSum);
