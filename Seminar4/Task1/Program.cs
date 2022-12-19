9ad4e75df78d96fcdfb28f66ca1c0a270af2ebb5
Elena <lenka.penka123412@gmail,com>
2022-12-19 16:41:49 +0300

    Вывод массива

--- Seminar4/Task1/Program.cs
+++ Seminar4/Task1/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// 
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for(int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(1, 11);
+Console.WriteLine($"[{string.Join(", ", array)}]");
