b7eb245c4da18aa89508e075880a5c0b847d8de3
Elena <lenka.penka123412@gmail,com>
2022-12-19 16:53:08 +0300

    Генерация дробных чисел

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Генерация дробных чисел
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for(int i = 0; i < array.Length; i++)
+{
+     array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
