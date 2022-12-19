186aeacdaee4f6f3b64d9c0caa8faff5bd2f7823
Elena <lenka.penka123412@gmail,com>
2022-12-19 19:02:05 +0300

    Факториал числа

---
+++ Seminar4/Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = (Convert.ToInt32(Console.ReadLine())), result = 1;
+for(int i = 2; i <= n; i++)
+{
+    result = result * i;
+}
+Console.Write(result);
