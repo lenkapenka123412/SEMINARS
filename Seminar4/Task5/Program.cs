4fee9cb9057d3dd0b527a73d03ee2e4aab96503e
Elena <lenka.penka123412@gmail,com>
2022-12-19 18:40:31 +0300

    Задача 24

---
+++ Seminar4/Task5/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 24
+
+/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.*/ 
+
+Console.Clear();
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((1 + n) / 2 * n);
