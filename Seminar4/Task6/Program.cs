fd52a5406a0bc0a66c3898bd78df3513ff834043
Elena <lenka.penka123412@gmail,com>
2022-12-19 18:50:08 +0300

    Задача 26

---
+++ Seminar4/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = ((byte)Convert.ToInt32(Console.ReadLine())), count = 0;
+while( n > 0)
+{
+    count++;
+    n /= 10;
+}
+Console.Write(count);
