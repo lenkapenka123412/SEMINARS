3afd63e0941c281366fc297ab6dcf19d3d4a84a5
Elena <lenka.penka123412@gmail,com>
2022-12-19 19:34:48 +0300

    Задача 25

--- Seminar4_DZ/Task1/Program.cs
+++ Seminar4_DZ/Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную 
+степень B.
+Нельзя использовать функцию Math.Pow
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Clear();
+
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i = 1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
