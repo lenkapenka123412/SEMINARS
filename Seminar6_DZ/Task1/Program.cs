f174a9960a9682c3f787751d2d8a3cb7a9deec4c
Elena <lenka.penka123412@gmail,com>
2022-12-28 12:50:55 +0300

    Задача 41

---
+++ Seminar6_DZ/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+Console.Write("Введите N чисел через пробел: ");
+int[] nambers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int sumPositivNamber = 0;
+foreach(int element in nambers)
+{
+    if(element > 0)
+    sumPositivNamber++;
+}
+Console.WriteLine($"Сумма положительных чисел: {sumPositivNamber}");
