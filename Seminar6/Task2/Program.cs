c4fc1bd089ee654b635d484950905cad49fbcf50
Elena <lenka.penka123412@gmail,com>
2022-12-28 11:05:30 +0300

    Суперсдвиг

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Суперсдвиг
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int k = Convert.ToInt32(Console.ReadLine());
+int[] result = new int[numbers.Length];
+k %= n;
+if (k > 0)
+{
+for (int i = 0; i < k; i++)
+result[i] = numbers[numbers.Length - k + i];
+for (int i = 0; i < numbers.Length - k; i++)
+result[k + i] = numbers[i];
+Console.WriteLine($"{string.Join(" ", result)}");
+}
+else
+{
+k = k * (-1);
+for (int i = 0; i < k; i++)
+result[numbers.Length - k + i] = numbers[i];
+for (int i = 0; i < numbers.Length - k; i++)
+result[i] = numbers[k + i];
+Console.WriteLine($"{string.Join(" ", result)}");
+}
