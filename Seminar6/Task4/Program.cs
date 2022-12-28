d9184e50a33734b8ff05edfc53121816985993b2
Elena <lenka.penka123412@gmail,com>
2022-12-28 11:41:44 +0300

    Проверка существования треугольника

---
+++ Seminar6/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник 
+с сторонами такой длины.*/
+
+Console.Clear();
+Console.WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if(a < b + c && b < a + c && c < a + c) // теорема о неравнестве треугольников
+Console.WriteLine("Треугольник");
+else
+Console.WriteLine("Нет");
