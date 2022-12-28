9ecb045c08de18b9a90590cbf16d645734101a00
Elena <lenka.penka123412@gmail,com>
2022-12-28 09:29:57 +0300

    Задача 38

---
+++ Stminar5_DZ/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным
+и минимальным элементов массива.
+*/
+
+void DiffArray(double[] array)
+{
+    double max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
+    Console.WriteLine($"Max = {max}");
+
+    double min = max;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+    }
+    Console.WriteLine($"Min = {min}");
+    Console.WriteLine($"Разница {max - min}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble() * 10, 2); 
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+DiffArray(array);
