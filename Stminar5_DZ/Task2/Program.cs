f68c7133c269f7a66d0c06e24c6b8718d5b9d4ed
Elena <lenka.penka123412@gmail,com>
2022-12-28 09:48:51 +0300

    Задача 36

--- Stminar5_DZ/Task2/Program.cs
+++ Stminar5_DZ/Task2/Program.cs
@@ -7,16 +7,9 @@
 void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(1, 10);
+        array[i] = new Random().Next(-10, 11);
 }
-void PrintArray(int[] numbers)
-{
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i] + " ");
-    }
-    Console.WriteLine();
-}
+
 Console.Clear();
 Console.Write("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -24,11 +17,11 @@ int[] numbers = new int[size];
 int count = 0;
 int result = 0;
 InputArray(numbers);
-PrintArray(numbers);
 
 for (int n = 0; n < numbers.Length; n++)
 {
     if (n % 2 != 0) result = result + numbers[n];
         count++;
 }
-Console.WriteLine(result);
+Console.WriteLine($"Вывод массива: [{string.Join(", ", numbers)}]");
+Console.WriteLine($"Сумма элементов,стоящих на нечетных позициях: {result}");
