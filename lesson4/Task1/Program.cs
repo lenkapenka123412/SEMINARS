2264f556cf84a7c72dfbbcb83e94cb33a4686bee
Elena <lenka.penka123412@gmail,com>
2023-01-04 13:15:53 +0300

    Двумрные массивы. Первый пример

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Двумерные массивы/ простой двумрный массив.
+
+string[, ] table = new string[3, 5];
+table[1, 2] = "слово";
+
+for(int rows = 0; rows < 2; rows++ )
+{
+    for(int colums = 0; colums < 5; colums++)
+    {
+        Console.WriteLine($"{table[rows, colums]}");
+    }
+}
