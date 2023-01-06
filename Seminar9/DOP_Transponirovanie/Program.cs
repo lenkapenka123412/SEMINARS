d924c84ca0638d1a89ebde3422514930a44daf63
Elena <lenka.penka123412@gmail,com>
2023-01-06 17:33:12 +0300

    Транспонирование ДОП

---
+++ Seminar9/DOP_Transponirovanie/Program.cs
@@ -0,0 +1,45 @@
+﻿// Транспонирование
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+            matrix[matrix.GetLength(0) - 1 - i, j] = temp;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив:");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+PrintMatrix(matrix);
